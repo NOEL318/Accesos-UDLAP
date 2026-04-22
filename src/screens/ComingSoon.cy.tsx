34d8182bb7a981338e6fe0092d6e86054c8f132d
NamelessKarn <ldanymgl@gmail.com>
2026-04-21 19:30:03 -0600

    fix #30 bug 3

--- src/screens/ComingSoon.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ComingSoon } from './ComingSoon'
-
-describe('<ComingSoon />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ComingSoon />)
-  })
-})
